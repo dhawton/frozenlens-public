28924e846458c571919c29786cb77539c11d0f1c
Daniel Hawton <daniel@hawton.org>
2019-05-27 22:24:21 -0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import url(https://fonts.googleapis.com/css?family=Courgette&display=swap);
+
+html {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+body {
+  background-image: url(./assets/background.jpg);
+  background-size: cover;
+  background-position: top left;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: Courgette, sans;
+}
+#app {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1>FrozenLens.org</h1>
+    <h3>Photography by Daniel A. Hawton</h3>
+    <p>Coming Soon.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
+
+<style scoped>
+.home {
+  flex: 1;
+  text-align: center;
+}
+.home h1 {
+  font-size: 36pt;
+}
+.home h1,
+.home h3 {
+  margin: 0;
+  padding: 0;
+  text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.5);
+}
+</style>
+
