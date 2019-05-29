87449912dc4ddd992f216e2dda6491fdae7338b0
Daniel Hawton <daniel@hawton.org>
2019-05-29 14:53:59 -0800

    pre-nuke

--- src/store.js
+++ src/store.js
@@ -1,18 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { API, graphqlOperation } from "aws-amplify";
+import { ListAlbums } from "queries";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: undefined
+    user: undefined,
+    albums: undefined
   },
   mutations: {
     user: (state, payload) => {
       state.user = payload;
+    },
+    albums: (state, payload) => {
+      state.albums = payload;
     }
   },
   getters: {
     user: state => state.user
+  },
+  actions: {
+    async getAlbums(context) {
+      let albums = await API.graphql(graphqlOperation(ListAlbums));
+      context.commit("albums", albums);
+    }
   }
 });
